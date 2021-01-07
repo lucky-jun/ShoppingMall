b6715ea6c3db4cdd5ca91a52019c67cf5683cc1f
lucky-jun <1154097841@qq.com>
2021-01-07 20:35:52 +0800

    20210107 2035

--- src/com/j/controller/test01.java
+++ src/com/j/controller/test01.java
@@ -12,32 +12,41 @@ import java.util.Map;
 public class test01 {
 	public static void main(String[] args) throws ParseException {
 
-		List<Integer> list1 = new ArrayList<Integer>();
-		list1.add(1);
-		list1.add(2);
-		list1.add(3);
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("username", "张三");
-		map.put("age",18);
-		map.put("maptest",list1);
-//		List<User> list2 = mapper.query(map);
+//		List<Integer> list1 = new ArrayList<Integer>();
+//		list1.add(1);
+//		list1.add(2);
+//		list1.add(3);
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		map.put("username", "张三");
+//		map.put("age",18);
+//		map.put("maptest",list1);
+////		List<User> list2 = mapper.query(map);
+//		
+//		
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		String str = "2020-12-12 12:12:00";
+//		Date parse = sdf.parse(str);
+//		System.out.println(parse);
+//		Date s = new Date();
+//		String format = sdf.format(s);
+//		System.out.println(format);
+//		Date parse2 = sdf.parse(format);
+//		System.out.println(parse2);
+//		String format2 = sdf.format(s);
+//		System.out.println("现在时间："+format2);
+//		Timestamp valueOf = Timestamp.valueOf(format2);
+//		System.out.println("----------:"+valueOf);
+//		java.sql.Timestamp t = new java.sql.Timestamp(s.getTime());
+//		
+//		System.out.println("Timestamp:"+t);
 		
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String str = "2020-12-12 12:12:00";
-		Date parse = sdf.parse(str);
-		System.out.println(parse);
-		Date s = new Date();
-		String format = sdf.format(s);
-		System.out.println(format);
-		Date parse2 = sdf.parse(format);
-		System.out.println(parse2);
-		String format2 = sdf.format(s);
-		System.out.println("现在时间："+format2);
-		Timestamp valueOf = Timestamp.valueOf(format2);
-		System.out.println("----------:"+valueOf);
-		java.sql.Timestamp t = new java.sql.Timestamp(s.getTime());
 		
-		System.out.println("Timestamp:"+t);
+		int i = -2147483647;
+		System.out.println(i);
+		System.out.println();
+		
+		
+		
 	}
 }
